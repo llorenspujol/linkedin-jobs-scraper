b5a186f3e1b022d45df6a9261c7b0a9a24c4224c
llorenspujol <llorenspujol1@gmail.com>
2023-10-20 17:41:16 +0200

    feat(workshop-step-1): initialize puppeteer

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,8 @@
 import * as path from 'path';
 import * as fs from 'fs';
 import * as puppeteer from 'puppeteer';
-import { concatMap, map } from 'rxjs/operators';
-import { defer } from 'rxjs';
 import { formatDate } from './utils';
 import yargs from 'yargs';
-import { getJobsFromLinkedin } from './linkedin';
-import { fromPromise } from 'rxjs/internal-compatibility';
 
 
 const argv = yargs(process.argv)
@@ -47,28 +43,5 @@ fs.mkdirSync(path.join(rootDirectory, jobsDataFolder), {recursive: true});
         ],
     });
 
-
-    getJobsFromLinkedin(browser).pipe(
-        concatMap(({jobs, searchParams}) => {
-            // Normally here you would save the jobs to a database, in this example just write the jobs in a json file.
-            const fileName = `linkedin_${searchParams.searchText}_${searchParams.locationText}_${searchParams.pageNumber}.json`
-            const logJobDataFile: string = path.join(rootDirectory, jobsDataFolder, fileName);
-            return defer(() => fromPromise(fs.promises.writeFile(logJobDataFile, JSON.stringify(jobs, null, 2), 'utf-8'))).pipe(
-                map(() => ({jobs, searchParams}))
-            )
-        })
-    ).subscribe(() => {}, (error) => {
-        console.log('Major error, closing browser...', error);
-        browser.close();
-        process.exit();
-    }, () => {
-        console.log('FINISHED');
-        browser.close();
-
-        setTimeout(() => {
-            console.log('PROCESS EXIT');
-            process.exit();
-        }, 0);
-    });
 
 })();
